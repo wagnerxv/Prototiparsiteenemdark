aa1e5a5830f192c75fa1d920102cb2d1e4283f20
wagnerxv <wagnerismohenrique@gmail.com>
2026-02-16 00:13:17 -0300

    fix: corrigir alguns erros bobos

--- site-enem-dark/src/components/ui/alert.tsx
+++ site-enem-dark/src/components/ui/alert.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { cva, type VariantProps } from "class-variance-authority@0.7.1";
+import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "./utils";
 
--- site-enem-dark/src/components/ui/sidebar.tsx
+++ site-enem-dark/src/components/ui/sidebar.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import * as React from "react";
-import { Slot } from "@radix-ui/react-slot@1.1.2";
-import { VariantProps, cva } from "class-variance-authority@0.7.1";
-import { PanelLeftIcon } from "lucide-react@0.487.0";
+import { Slot } from "@radix-ui/react-slot";
+import { VariantProps, cva } from "class-variance-authority";
+import { PanelLeftIcon } from "lucide-react";
 
-import { useIsMobile } from "./use-mobile";
-import { cn } from "./utils";
+import { useIsMobile } from "@/lib/use-mobile";
+import { cn } from "@/lib/utils";
 import { Button } from "./button";
 import { Input } from "./input";
 import { Separator } from "./separator";
